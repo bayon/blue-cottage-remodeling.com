61a8773de7eeae5eb4b88dc333153fa33bc39723
Bayon Forte <bayon@forteworks.com>
2021-02-18 18:34:38 -0500

    gain some control over MuiTheme...

--- blue-cottage/src/App.js
+++ blue-cottage/src/App.js
@@ -1,13 +1,13 @@
-import 'semantic-ui-css/semantic.min.css';
-import './App.css';
-import Routing from './components/Routing';
- 
+import "semantic-ui-css/semantic.min.css";
+import "./App.css";
+import Routing from "./components/Routing";
 
 function App() {
   return (
-    <div>
-     <Routing/>
-    </div>
+        < >
+          <Routing />
+        </>
+   
   );
 }
 
--- blue-cottage/src/components/Routing.js
+++ blue-cottage/src/components/Routing.js
@@ -1,63 +1,83 @@
 //
-import {
-  Container,
-  Drawer,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-  Typography
-} from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
-import HomeIcon from "@material-ui/icons/Home";
-import InfoIcon from "@material-ui/icons/Info";
+import { Container, Typography } from "@material-ui/core";
+import AppBar from "@material-ui/core/AppBar";
+import Button from "@material-ui/core/Button";
+import { blue } from "@material-ui/core/colors";
+import IconButton from "@material-ui/core/IconButton";
+import { createMuiTheme, makeStyles, ThemeProvider } from "@material-ui/core/styles";
+import Toolbar from "@material-ui/core/Toolbar";
+import MenuIcon from "@material-ui/icons/Menu";
 import React from "react";
 import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
- 
+
+
+
+const theme = createMuiTheme({
+  palette: {
+    primary:blue,
+  }
+})
+
 const useStyles = makeStyles((theme) => ({
   drawerPaper: { width: "inherit" },
   link: {
     textDecoration: "none",
-    color: theme.palette.text.primary,
+    // color: theme.palette.text.primary,
   },
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  button: {
+    backgroundColor: 'white',
+    [theme.breakpoints.up('sm')]:{
+      backgroundColor: 'black'
+    }
+  }
 }));
 
 export default function Routing() {
   const classes = useStyles();
 
   return (
+  
+   <ThemeProvider theme={theme}>
     <Router>
       <>
-        <Drawer
-          style={{ width: "220px" }}
-          variant="persistent"
-          anchor="left"
-          open={true}
-          classes={{ paper: classes.drawerPaper }}
-        >
-          <List>
-            <Link to="/">
-              <ListItem button>
-                <ListItemIcon>
-                  <HomeIcon />
-                </ListItemIcon>
-                <ListItemText primary={"Home"} />
-              </ListItem>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Link to="/home">
+              <Typography variant="h6" className={classes.title}>
+                Home
+              </Typography>
             </Link>
-
             <Link to="/about">
-              <ListItem button>
-                <ListItemIcon>
-                  <InfoIcon />
-                </ListItemIcon>
-                <ListItemText primary={"About"} />
-              </ListItem>
+              <Typography variant="h6" className={classes.title}>
+                About
+              </Typography>
             </Link>
-          </List>
-        </Drawer>
+            <Link to="/login">
+              <Button color="inherit"  className={classes.button}>Login</Button>
+            </Link>
+          </Toolbar>
+        </AppBar>
+        
         <Switch>
-          <Route exact path="/">
-            <Container>
+          <Route exact path="/home">
+            <Container maxWidth="sm">
               <Typography variant="h3" gutterBottom>
                 Blue Cottage Remodeling
               </Typography>
@@ -73,7 +93,7 @@ export default function Routing() {
             </Container>
           </Route>
           <Route exact path="/about">
-            <Container>
+            <Container maxWidth="sm">
               <Typography variant="h3" gutterBottom>
                 About
               </Typography>
@@ -88,9 +108,14 @@ export default function Routing() {
               </Typography>
             </Container>
           </Route>
+          <Route exact path="/login">
+            <Container maxWidth="sm">login</Container>
+          </Route>
         </Switch>
       </>
     </Router>
+    </ThemeProvider>
+  
   );
 }
 
